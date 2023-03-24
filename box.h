539bc1557fae67bd80a6b48bc1adb2f25dd922c3
juliannea <112399053+juliannea@users.noreply.github.com>
2023-03-23 22:56:31 -0400

    Update box.h

--- box.h
+++ box.h
@@ -1,10 +1,11 @@
 #pragma once 
 #include <iostream>
+#include "imageio.h"
 
 void white_box(std::string );
 
 void frame(std::string );
 
-void scale(std::string );
+void scale(std::string , int image[MAX_H][MAX_W]);
 
 void pixel(std::string );
